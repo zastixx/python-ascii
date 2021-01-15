e3f0be11cfc3d3e0853436772c7c172fb5fd2563
TARUN KUMAR UTTAM <57785497+TARUN387KUMARUTTAM@users.noreply.github.com>
2021-01-15 23:42:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 import ascii_magic
-output = ascii_magic.from_image_file(
-    'fun.jpeg',
-    columns=200,
-    back=ascii_magic.Back.BLUE
-)
+output = ascii_magic.from_image_file("fun.jpeg",columns=80,char="$*")
+ascii_magic.Modes.ascii(output)
